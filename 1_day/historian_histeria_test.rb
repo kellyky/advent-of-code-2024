ce62d9d9389a4f3742e09ec04da6491034b832e3
Kelly Popko <kelpopko@gmail.com>
2024-12-01 01:37:56 -0600

    Add solution day 1, part 2 🎉🎄

--- 1_day/historian_histeria_test.rb
+++ 1_day/historian_histeria_test.rb
@@ -3,16 +3,29 @@ require_relative 'historian_histeria'
 
 class HistorianHisteriaTest< Minitest::Test
   # Part 1
-  def test_sample
+  def test_total_distance_sample
     file = 'sample-input.txt'
     historian_search = HistorianHisteria.new(file)
     assert_equal 11, historian_search.total_distance
   end
 
-  def test_real_input
+  def test_total_distance_real_input
     file = 'input.txt'
     historian_search = HistorianHisteria.new(file)
     assert_equal 1110981, historian_search.total_distance
+  end
+
+  # Part2
+  def test_similarity_score_sample
+    file = 'sample-input.txt'
+    historian_search = HistorianHisteria.new(file)
+    assert_equal 31, historian_search.similarity_score
+  end
+
+  def test_similarity_score_real_input
+    file = 'input.txt'
+    historian_search = HistorianHisteria.new(file)
+    assert_equal 24869388, historian_search.similarity_score
   end
 end
 
